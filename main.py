5dd0f2284deb609f272ada8acfd3ac05fdfa34fa
TephdyTech <justsepf24@gmail.com>
2023-10-12 22:38:56 +0800

    minor changes

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ def show_hide():
     add_btn_var.get()
 
     if text_frame.winfo_viewable():
+        search_bar_frame.pack(fill='x', padx=15, pady=15)
         text_frame.pack_forget()
         add_btn_var.set("ADD NOTES")
+        task_main_frame.pack(fill="both", ipadx=10, ipady=10, padx=10, pady=10, expand=True)
     else:
-        text_frame.pack()
+        text_frame.pack(padx=15, pady=15)
         add_btn_var.set("BACK")
         text_area.delete("1.0", "end")
+        task_main_frame.pack_forget()
+        search_bar_frame.pack_forget()
 
 def cancel_btn():
+    search_bar_frame.pack(fill='x',
+                          padx=15,
+                          pady=15)
     text_frame.pack_forget()
     add_btn_var.set("ADD NOTES")
     text_area.delete("1.0","end")
+    task_main_frame.pack(fill="both",
+                         ipadx=10,
+                         ipady=10,
+                         padx=10,
+                         pady=10,
+                         expand=True)
 
 def add_task():
+    search_bar_frame.pack(fill='x',
+                          padx=15,
+                          pady=15)
     task_value=text_area.get("1.0", "end-1c")
     # print(task_value)
     task.append(task_value)
@@ -28,6 +44,12 @@ def add_task():
     text_area.delete("1.0","end")
     text_frame.pack_forget()
     add_btn_var.set("ADD NOTES")
+    task_main_frame.pack(fill="both",
+                         ipadx=10,
+                         ipady=10,
+                         padx=10,
+                         pady=10,
+                         expand=True)
 
 
 task=[]
@@ -37,11 +59,11 @@ window=ttb.Window()
 window.geometry('500x800')
 window.title("pyWRITE")
 window.resizable(False, False)
-style=Style(theme="darkly")
+style=Style(theme="minty")
 
 # main_frame
 main_frame=ttb.Frame(window)
-main_frame.pack(expand=True, fill='both', ipadx=10, ipady=10)
+main_frame.pack(expand=True, fill='both', ipadx=20, ipady=10)
 
 # header_frame
 header_frame=ttb.Frame(main_frame)
@@ -64,15 +86,15 @@ header_separator.pack(fill='x')
 
 # text_frame
 text_frame=ttb.Frame(main_frame)
-# text_frame.pack(fill='both')
+# text_frame.pack(fill='both') function will pack this
 
 # text_frame_label
 text_frame_label=ttb.Label(text_frame, text="ADD NOTES")
 text_frame_label.pack()
 
 # text_area
-text_area=ttb.Text(text_frame)
-text_area.pack(ipadx=20, ipady=20)
+text_area=ttb.Text(text_frame, wrap="word")
+text_area.pack()
 
 # text_area_control
 text_area_control=ttb.Frame(text_frame)
@@ -89,6 +111,45 @@ cancel_btn.grid(row=0, column=1, sticky="ew")
 
 for widget in text_area_control.winfo_children():
     widget.grid_configure(padx=3, pady=3)
+
+
+# search_bar_frame
+search_bar_frame=ttb.Frame(main_frame)
+search_bar_frame.pack(fill='x', padx=15, pady=15)
+
+# search_bar
+search_bar=ttb.Entry(search_bar_frame)
+search_bar.insert(0, "Search")
+search_bar.bind("<FocusIn>", lambda e: search_bar.delete('0', 'end'))
+search_bar.pack(fill='x')
+
+
+# task_main_frame
+task_main_frame=ttb.Frame(main_frame, height=10)
+task_main_frame.pack(fill="x", padx=15)
+
+# task_content_frame
+task_content_frame=ttb.Frame(task_main_frame)
+task_content_frame.pack(fill='x', padx=10, pady=10)
+
+# task_label
+task_label=ttb.Label(task_content_frame, text="Note 1", font="Calibre, 12 bold")
+task_label.pack(fill='x', pady=4)
+
+# task_sub_label
+task_sub_label=ttb.Label(task_content_frame, text="11, October", font="Calibre 9", style="secondary")
+task_sub_label.pack(fill='x', pady=4)
+
+# task_content
+task_content=ttb.Label(task_content_frame,
+                       text="Hello World",
+                       font="Calibre 10")
+task_content.pack(fill='x', pady=4)
+
+# task_separator
+task_separator=ttb.Separator(task_content_frame, style="success")
+task_separator.pack(fill='x', pady=20)
+
 
 # run
 window.mainloop()
