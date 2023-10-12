ed0a1b684484b50583ad08e1a2ee2c52f25dad27
TephdyTech <justsepf24@gmail.com>
2023-10-12 16:41:39 +0800

    edited header

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ window=ttb.Window()
 window.geometry('500x800')
 window.title("To-Do List")
 window.resizable(False, False)
-style=Style()
+style=Style(theme="darkly")
 
 # main_frame
 main_frame=ttb.Frame(window)
@@ -22,13 +22,8 @@ header_frame.pack(fill='x', padx=10, pady=10)
 header_title_label=ttb.Label(header_frame, text="To-Do List", font="Calibre 14 bold")
 header_title_label.pack(side="left")
 
-# # add icon
-# add_icon=ttk.PhotoImage(file="icons/plus-square-fill.png")
-# plus_icon=ttb.Label(header_frame, text="Add")
-# plus_icon.pack(side="right")
-
 # add_btn
-add_btn=ttb.Button(header_frame, text="Add", width=10)
+add_btn=ttb.Button(header_frame, text="Add Task", width=10, style="success")
 add_btn.pack(side="right")
 
 #
