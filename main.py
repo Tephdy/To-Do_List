afd10bde9413844a6d094f70a90e98e98f596dae
TephdyTech <justsepf24@gmail.com>
2023-10-12 16:06:19 +0800

    created main fram

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello Git")
+# modules
+import tkinter as ttk
+import ttkbootstrap as ttb
+from ttkbootstrap import Style
+
+# window
+window=ttb.Window()
+window.geometry('500x800')
+window.title("To-Do List")
+style=Style()
+
+# run
+window.mainloop()
